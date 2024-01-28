dd05917b470422667bee48b28fd63fdcfbb10625
Igor Zarzycki <izarzycki@mailfence.com>
2024-01-29 00:52:23 +0100

    Add answer composition in superblock.

--- src/superblock.h
+++ src/superblock.h
@@ -1,29 +1,34 @@
 #pragma once
 
+#include "compose.h"
+#include "content.h"
+#include "global_config.h"
 #include "system_config.h"
 
 #include <cstdint>
+#include <memory>
+#include <utility>
 
-template <system_config System> class superblock
+template <system_config System, global_config Config> class superblock
 {
  constexpr static bool has_inodes_count() noexcept;
  constexpr static bool has_lpf_ino() noexcept;
  public:
- using inodes_count_t = std :: uint32_t;
- using lpf_ino_t = std :: uint32_t;
- inodes_count_t get_inodes_count() const noexcept requires(has_inodes_count());
- lpf_ino_t get_lpf_ino() const noexcept requires(has_lpf_ino());
+ template <class Self> auto get_inodes_count(this Self&& self) noexcept requires(has_inodes_count());
+ template <class Self> auto get_lpf_ino(this Self&& self) noexcept requires(has_lpf_ino());
+ private:
+ constexpr const static std :: size_t total_size = 1024zu;
+ std :: shared_ptr<content<total_size, Config> > block;
 };
 
-template <system_config System> using superblock_inodes_count_t = typename superblock <System> :: inodes_count_t;
-template <system_config System> using superblock_lpf_ino_t = typename superblock <System> :: lpf_ino_t; 
-
-template <system_config System> constexpr inline bool superblock <System> :: has_inodes_count() noexcept
+template <system_config System, global_config Config>
+constexpr inline bool superblock <System, Config> :: has_inodes_count() noexcept
 {
  return true;
 }
 
-template <system_config System> constexpr inline bool superblock <System> :: has_lpf_ino() noexcept
+template <system_config System, global_config Config>
+constexpr inline bool superblock <System, Config> :: has_lpf_ino() noexcept
 {
  switch (System.file_system)
  {
@@ -37,16 +42,16 @@ template <system_config System> constexpr inline bool superblock <System> :: has
  }
 }
 
-template <system_config System>
-inline superblock_inodes_count_t<System>
-superblock <System> :: get_inodes_count() const noexcept requires(superblock <System> :: has_inodes_count())
+template <system_config System, global_config Config> template <class Self>
+inline auto superblock <System, Config> :: get_inodes_count(this Self&& self) noexcept
+requires(superblock <System, Config> :: has_inodes_count())
 {
- return {};
+ return compose<0x003zu, 0x002zu, 0x001zu, 0x000zu>(std :: forward<Self>(self).block);
 }
 
-template <system_config System>
-inline superblock_lpf_ino_t<System>
-superblock <System> :: get_lpf_ino() const noexcept requires(superblock <System> :: has_lpf_ino())
+template <system_config System, global_config Config> template <class Self>
+inline auto superblock <System, Config> :: get_lpf_ino(this Self&& self) noexcept
+requires(superblock <System, Config> :: has_lpf_ino())
 {
- return {};
+ return compose<0x26B, 0x26A, 0x269, 0x268>(std :: forward<Self>(self).block);
 }
