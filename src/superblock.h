bc1eb944a6a903fd29288d9b6b9d279f62da0e3c
Igor Zarzycki <izarzycki@mailfence.com>
2024-01-29 18:33:24 +0100

    Add reading creator OS field.

--- src/superblock.h
+++ src/superblock.h
@@ -13,10 +13,12 @@
 template <system_config System, global_config Config> class superblock
 {
  constexpr static bool has_inodes_count() noexcept;
+ constexpr static bool has_creator_os() noexcept;
  constexpr static bool has_rev_level() noexcept;
  constexpr static bool has_lpf_ino() noexcept;
  public:
  template <class Self> auto get_inodes_count(this Self&& self) noexcept requires(has_inodes_count());
+ template <class Self> auto get_creator_os(this Self&& self) noexcept requires(has_creator_os());
  template <class Self> auto get_rev_level(this Self&& self) noexcept requires(has_rev_level());
  template <class Self> auto get_lpf_ino(this Self&& self) noexcept requires(has_lpf_ino());
  private:
@@ -26,6 +28,12 @@ template <system_config System, global_config Config> class superblock
 
 template <system_config System, global_config Config>
 constexpr inline bool superblock <System, Config> :: has_inodes_count() noexcept
+{
+ return true;
+}
+
+template <system_config System, global_config Config>
+constexpr inline bool superblock <System, Config> :: has_creator_os() noexcept
 {
  return true;
 }
@@ -59,6 +67,36 @@ requires(superblock <System, Config> :: has_inodes_count())
 }
 
 template <system_config System, global_config Config> template <class Self>
+inline auto superblock <System, Config> :: get_creator_os(this Self&& self) noexcept
+requires(superblock <System, Config> :: has_creator_os())
+{
+ if (const auto os = compose<0x04Bzu, 0x04Azu, 0x049zu, 0x048zu>(std :: forward<Self>(self).block); !os)
+ {
+  logger<Config, log_level :: error> l; l.log("Failed to read creator OS.");
+  return static_cast<std :: optional<system_config :: creator_os_t> >(std :: nullopt);
+ }
+ else switch (*os)
+ {
+  using enum system_config :: creator_os_t;
+  case UINT32_C(0):
+  return std :: make_optional<system_config :: creator_os_t>(linux);
+  case UINT32_C(1):
+  return std :: make_optional<system_config :: creator_os_t>(hurd);
+  case UINT32_C(2):
+  return std :: make_optional<system_config :: creator_os_t>(masix);
+  case UINT32_C(3):
+  return std :: make_optional<system_config :: creator_os_t>(free_bsd);
+  case UINT32_C(4):
+  return std :: make_optional<system_config :: creator_os_t>(lites);
+  default:
+  {
+   logger<Config, log_level :: error> l; l.log("Unknown creator OS: ", std :: hex, *os);
+   return static_cast<std :: optional<system_config :: creator_os_t> >(std :: nullopt);
+  }
+ }
+}
+
+template <system_config System, global_config Config> template <class Self>
 inline auto superblock <System, Config> :: get_rev_level(this Self&& self) noexcept
 requires(superblock <System, Config> :: has_rev_level())
 {
@@ -88,3 +126,4 @@ requires(superblock <System, Config> :: has_lpf_ino())
 {
  return compose<0x26Bzu, 0x26Azu, 0x269zu, 0x268zu>(std :: forward<Self>(self).block);
 }
+
