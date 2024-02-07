4632fad754564901fadb2b8f962b7f50d27cce5b
Igor Zarzycki <izarzycki@mailfence.com>
2024-02-08 00:24:27 +0100

    Add initial feature_compat flags.

--- src/system_config.h
+++ src/system_config.h
@@ -9,7 +9,12 @@ struct system_config
  creator_os_t creator_os;
  revision_t revision;
  bool feature_compat_64bit;
+ template <file_system_t> struct feature_compat_flags;
  constexpr static system_config getDefault() noexcept;
+ private:
+ enum class ext2_feature_compat_flags : std :: uint32_t;
+ enum class ext3_feature_compat_flags : std :: uint32_t;
+ enum class ext4_feature_compat_flags : std :: uint32_t;
 };
 
 enum class system_config :: file_system_t
@@ -35,6 +40,50 @@ enum class system_config :: revision_t
  dynamic_rev
 };
 
+enum class system_config :: ext2_feature_compat_flags : std :: uint32_t
+{
+ dir_prealloc = UINT32_C(0b000001),
+ imagic_inodes = UINT32_C(0b000010),
+ has_journal = UINT32_C(0b000100), //Marks at least ext3.
+ ext_attr = UINT32_C(0b001000),
+ resize_inode = UINT32_C(0b010000),
+ dir_index = UINT32_C(0b100000)
+};
+
+enum class system_config :: ext3_feature_compat_flags : std :: uint32_t
+{
+};
+
+enum class system_config :: ext4_feature_compat_flags : std :: uint32_t
+{
+ lazy_bg = UINT32_C(0b0001000000),
+ exclude_inode = UINT32_C(0b0010000000),
+ exclude_bitmap = UINT32_C(0b0100000000),
+ sparse_super = UINT32_C(0b1000000000)
+};
+
+template <> struct system_config :: feature_compat_flags<system_config :: file_system_t :: ext>
+{
+};
+
+template <> struct system_config :: feature_compat_flags<system_config :: file_system_t :: ext2>
+{
+ using enum system_config :: ext2_feature_compat_flags;
+};
+
+template <> struct system_config :: feature_compat_flags<system_config :: file_system_t :: ext3>
+{
+ using enum system_config :: ext2_feature_compat_flags;
+ using enum system_config :: ext3_feature_compat_flags;
+};
+
+template <> struct system_config :: feature_compat_flags<system_config :: file_system_t :: ext4>
+{
+ using enum system_config :: ext2_feature_compat_flags;
+ using enum system_config :: ext3_feature_compat_flags;
+ using enum system_config :: ext4_feature_compat_flags;
+};
+
 constexpr inline system_config system_config :: getDefault() noexcept
 {
  return
@@ -45,4 +94,3 @@ constexpr inline system_config system_config :: getDefault() noexcept
   .feature_compat_64bit = false
  };
 }
-
