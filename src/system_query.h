1c6403b5dc16a5bed385bdc74cf22a50340a1595
Igor Zarzycki <izarzycki@mailfence.com>
2024-02-17 22:41:28 +0100

    Add system queries.

---
+++ src/system_query.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "system_config.h"
+
+template <system_config System> class system_query
+{
+ ~system_query() = delete;
+ public:
+ constexpr static bool is_64bit() noexcept;
+ constexpr static bool is_having_block_preallocation() noexcept;
+};
+
+template <system_config System>
+constexpr inline bool system_query <System> :: is_64bit() noexcept
+{
+ switch (System.file_system)
+ {
+  using enum system_config :: file_system_t;
+  case ext:
+  case ext2:
+  case ext3:
+  return false;
+  case ext4:
+  return is_flag_set<System, system_config :: feature_incompat_flags <ext4> :: bits64>();
+ }
+}
+
+template <system_config System>
+constexpr inline bool system_query <System> :: is_having_block_preallocation() noexcept
+{
+ switch (System.file_system)
+ {
+  using enum system_config :: file_system_t;
+  case ext:
+  return false;
+  case ext2:
+  return is_flag_set<System, system_config :: feature_compat_flags <ext2> :: dir_prealloc>();
+  case ext3:
+  return is_flag_set<System, system_config :: feature_compat_flags <ext3> :: dir_prealloc>();
+  case ext4:
+  return is_flag_set<System, system_config :: feature_compat_flags <ext4> :: dir_prealloc>();
+ }
+}
