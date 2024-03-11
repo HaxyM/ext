b983dc691f1ce9998211d1675e497097dc9762dc
Igor Zarzycki <izarzycki@mailfence.com>
2024-03-11 17:47:00 +0100

    Add case fold to system query.

--- src/system_query.h
+++ src/system_query.h
@@ -8,6 +8,7 @@ template <system_config System> class system_query
  public:
  constexpr static bool is_64bit() noexcept;
  constexpr static bool is_having_block_preallocation() noexcept;
+ constexpr static bool is_having_case_fold() noexcept
 };
 
 template <system_config System>
@@ -39,5 +40,20 @@ constexpr inline bool system_query <System> :: is_having_block_preallocation() n
   return is_flag_set<System, system_config :: feature_compat_flags <ext3> :: dir_prealloc>();
   case ext4:
   return is_flag_set<System, system_config :: feature_compat_flags <ext4> :: dir_prealloc>();
+ }
+}
+
+template <system_config System>
+constexpr inline bool system_query <System> :: is_having_case_fold() noexcept
+{
+ switch (System.file_system)
+ {
+  using enum system_config :: file_system_t;
+  case ext:
+  case ext2:
+  case ext3:
+  return false;
+  case ext4:
+  return is_flag_set<System, system_config :: feature_incompat_flags <ext4> :: case_fold>();
  }
 }
