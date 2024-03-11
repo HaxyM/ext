ab14be55cb64e022a952dc9926b61de936f28913
Igor Zarzycki <izarzycki@mailfence.com>
2024-03-11 18:14:53 +0100

    Add encoding parmas masks.

--- src/system_query.h
+++ src/system_query.h
@@ -8,7 +8,8 @@ template <system_config System> class system_query
  public:
  constexpr static bool is_64bit() noexcept;
  constexpr static bool is_having_block_preallocation() noexcept;
- constexpr static bool is_having_case_fold() noexcept
+ constexpr static bool is_having_case_fold() noexcept;
+ constexpr static auto get_encoding_params_mask() noexcept;
 };
 
 template <system_config System>
@@ -55,5 +56,19 @@ constexpr inline bool system_query <System> :: is_having_case_fold() noexcept
   return false;
   case ext4:
   return is_flag_set<System, system_config :: feature_incompat_flags <ext4> :: case_fold>();
+ }
+}
+
+template <system_config System>
+constexpr inline auto system_query <System> :: get_encoding_params_mask() noexcept
+{
+ using enum system_config :: encoding_t;
+ if constexpr (System.encoding == ascii)
+ {
+  return system_config :: encoding_params_t :: ascii_mask;
+ }
+ else if constexpr (System.encoding == utf8_12_1)
+ {
+  return system_config :: encoding_params_t :: utf8_12_1_mask;
  }
 }
